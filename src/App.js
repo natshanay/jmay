b6e1859adb1cde529ded64a8fde423fca87b37c8
Natnael Chala <natnaelchala10@gmail.com>
2024-11-27 12:30:53 -0800

    naty

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './Header';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+{/* <Router>
+      <Route path="header" 
+      element={
+        }
+        />
+        <Hero/>
+        </Router>
+        */}
+        <Header/>
+        <Hero/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter ,Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+      <div>
+    
+
+      <div className="header">
+        <Link to="/about">About</Link>
+      </div>
+    
+</div>
+    
+      
+        
+  );
+};
+
+export default Header;
